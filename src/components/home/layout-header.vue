59a0f166fea57fe715aa648cf41e7c1666025638
xiehangyun <631978355@qq.com>
2019-12-19 18:10:52 +0800

    主页home-头部header完成，登陆校验完成

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -1,13 +1,77 @@
 <template>
-  <div>头部</div>
+  <el-row class="layout-header" type="flex" align="middle" justify="space-between">
+    <el-col :span="6" class="left">
+      <i class="el-icon-s-unfold"></i>
+      <span class="title">江苏传智播客教育科技股份有限公司</span>
+    </el-col>
+    <el-col :span="4" class="right">
+      <el-row type="flex" align="middle" justify="end">
+          <img :src="userInfo.photo || defaultImg" alt="">
+          <el-dropdown trigger="click" @command="userHandle">
+              <span>{{userInfo.name}}<i class="el-icon-arrow-down el-icon--right"></i></span>
+              <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                  <el-dropdown-item command="git">git地址</el-dropdown-item>
+                  <el-dropdown-item command="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+          </el-dropdown>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      userInfo: {},
+      defaultImg: require('../../assets/img/header.jpg')
+    }
+  },
+  created () {
+    let token = window.localStorage.getItem('user-token')
+    this.$axios({
+      url: `/user/profile`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(result => {
+      this.userInfo = result.data.data
+    }).catch(() => {
+      this.$router.push('/login')
+    })
+  },
+  methods: {
+    userHandle (command) {
+      if (command === 'logout') {
+        window.localStorage.removeItem('user-token')
+        this.$router.push('/login')
+      } else if (command === 'git') {
+        window.location.href = 'https://github.com/xiehangyun'
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.layout-header {
+  height: 60px;
+  .left {
+    font-size: 18px;
+    .title {
+      margin-left: 4px;
+      color: #2c3e50;
+      font-size: 16px;
+    }
+  }
+  .right {
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      margin-right: 5px;
+    }
+  }
+}
 </style>
